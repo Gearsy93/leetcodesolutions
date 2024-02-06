782e27b43338d2bc0d4ef0440c560a62f45afa65
Sergey P. <sergey.pistsov@ural.rt.ru>
2024-02-06 23:17:14 +0500

    Fixed LeetCode 75/Can Place Flowers - 605.cs

--- LeetCode 75/Can Place Flowers - 605.cs
+++ LeetCode 75/Can Place Flowers - 605.cs
@@ -9,18 +9,25 @@
 /* Solution:
  * To find number of plantabled flowerbed, for each empty flowerbed streak find its max count of plantable plants:
  * (emptyStreak % 2 == 0) ? (emptyStreak - 1) / 2 : emptyStreak / 2;
+ * + count bounds...
  */
 public class Solution {
     public bool CanPlaceFlowers(int[] flowerbed, int n) {
-        var plantable = 0;
+        if (flowerbed.Length == 1 && flowerbed[0] == 0) return true;
+        var plantable   = 0;
         var emptyStreak = 0;
         for (int i = 0; i < flowerbed.Length; i++) {
             if (flowerbed[i] == 0) {
                 emptyStreak++;
+                if (i == flowerbed.Length - 1) {
+                    if (emptyStreak > 1 && (emptyStreak % 2 == 0 || emptyStreak  - 1 == i)) plantable += 1;
+                    plantable += (emptyStreak           % 2 == 0) ? (emptyStreak - 1) / 2 : emptyStreak / 2;
+                } 
             }
             else {
-                plantable += (emptyStreak % 2 == 0) ? (emptyStreak - 1) / 2 : emptyStreak / 2;
-                emptyStreak = 0;
+                if (emptyStreak == i && emptyStreak > 1 && emptyStreak    % 2 == 0) plantable += 1;
+                plantable   += (emptyStreak % 2 == 0) ? (emptyStreak - 1) / 2 : emptyStreak / 2;
+                emptyStreak =  0;
             }
         }
         return plantable >= n;
